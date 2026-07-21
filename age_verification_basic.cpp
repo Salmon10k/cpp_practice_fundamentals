6900f7161e933f7060859971a9624541b9450d40
Salmon10k <salmanmalik2005@yahoo.com>
2026-07-21 11:27:35 +0500

    age verification basic cpp

---
+++ age_verification_basic.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+int main() {
+int age;
+std::cout <<"what is your age? =  ";
+std::cin >> age;
+
+if (age >=18){
+    std::cout <<"welcome to the site , its gon be good my fellow african american!";
+
+}
+else if (age<0 ){
+    std::cout<<"you haven't been born yet , stop messing around";
+}
+else {
+   std::cout <<"your are not old enough to enter this site dummy , go do your homework";
+
+}
+
+
+}
