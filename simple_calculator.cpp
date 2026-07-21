f6c2f5879fb6b8c02332b880d0b5e2b82acc6323
Salmon10k <salmanmalik2005@yahoo.com>
2026-07-21 16:00:13 +0500

    added the division by 0 error

--- simple_calculator.cpp
+++ simple_calculator.cpp
@@ -15,7 +15,14 @@ switch(op){
  case '+': result=num1+num2; std::cout<<"result is "<<result << '\n'; break;
  case '-': result=num1-num2; std::cout<<"result is "<<result << '\n'; break;
  case '*': result=num1*num2; std::cout<<"result is "<<result << '\n'; break;
- case '/': result=num1/num2; std::cout<<"result is "<<result << '\n'; break;
+ case '/':
+    if (num2 != 0) {
+        result = num1 / num2;
+        std::cout << "result is " << result << '\n';
+    } else {
+        std::cout << "ERROR! Division by zero is not possible.\n";
+    }
+    break;
 default: std::cout<<" ERROR! invalid input";
 }
 std::cout<<"####################################";
